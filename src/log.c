17a033e74cfbead4eeaa8728926acd812a162a2f
Conrad Parker <conrad@metadecks.org>
2009-04-29 00:40:44 +0900

    log startup and shutdown times

--- src/log.c
+++ src/log.c
@@ -1,3 +1,7 @@
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
 #include <stdio.h>
 
 #include "params.h"
@@ -6,14 +10,29 @@
 #define ACCESS_LOG "/var/log/sighttpd/access.log"
 #define ERROR_LOG "/var/log/sighttpd/error.log"
 
-#define LOG_FMT "[%s] \"%s\" 200 %s \"%s\"\r\n"
+#define DATE_FMT "[%s] "
+#define LOG_FMT DATE_FMT "\"%s\" 200 %s \"%s\"\r\n"
 
 FILE * access_log=NULL, * error_log=NULL;
+
+static int log_date (FILE * stream)
+{
+        char date[256];
+
+        httpdate_snprint (date, 256, time(NULL));
+        return fprintf (stream, DATE_FMT, date);
+}
 
 int log_open (void)
 {
         access_log = fopen (ACCESS_LOG, "a");
         error_log = fopen (ERROR_LOG, "a");
+
+        if (error_log != NULL) {
+                log_date (error_log);
+                fprintf (error_log, "Sighttpd/" VERSION " resuming normal operations\n");
+                fflush (error_log);
+        }
 }
 
 int log_close (void)
@@ -23,6 +42,8 @@ int log_close (void)
                 fclose (access_log);
         }
         if (error_log != NULL) {
+                log_date (error_log);
+                fprintf (error_log, "Sighttpd/" VERSION " shutting down\n");
                 fflush (error_log);
                 fclose (error_log);
         }
