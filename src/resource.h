438bbcd4dadf0cdcb531d296a6602d0b105fc20d
Conrad Parker <conrad@metadecks.org>
2010-04-07 13:40:59 +0900

    update ResourceDelete type

--- src/resource.h
+++ src/resource.h
@@ -9,7 +9,7 @@ typedef int (*ResourceCheck) (http_request * request, void * data);
 typedef void (*ResourceHead) (http_request * request, params_t * request_headers,
 		const char ** status_line, params_t ** response_headers, void * data);
 typedef void (*ResourceBody) (int fd, http_request * request, params_t * request_headers, void * data);
-typedef int (*ResourceDelete) (void * data);
+typedef void (*ResourceDelete) (void * data);
 
 struct resource {
 	ResourceCheck check;
